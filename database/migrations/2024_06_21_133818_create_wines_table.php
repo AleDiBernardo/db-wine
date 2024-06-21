b0ad91486b9f70aee8e715c14c7033c6239a8b11
OmarThierno <thiernomarndiaye76@gmail.com>
2024-06-21 17:29:45 +0200

    feat: add index file  and crud function

--- database/migrations/2024_06_21_133818_create_wines_table.php
+++ database/migrations/2024_06_21_133818_create_wines_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('wines', function (Blueprint $table) {
             $table->id();
+            $table->string('slug')->unique();
             $table->string('wine');
             $table->string('review');
             $table->float('average', 2, 1);
